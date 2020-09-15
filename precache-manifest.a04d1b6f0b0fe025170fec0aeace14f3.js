6ab3fcdf318ee5fe2e0bca05fd568105c721cfdc
marc-sabeh <sabehmarc@outlook.com>
2020-09-16 01:20:00 +0300

    Updates

---
+++ precache-manifest.a04d1b6f0b0fe025170fec0aeace14f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a27d08e8a6c97d82520de01bf8ae4f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "24e46c297e7e080745be",
+    "url": "/robofriends/static/css/main.3b7c2799.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e46c297e7e080745be",
+    "url": "/robofriends/static/js/main.18fc7183.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
